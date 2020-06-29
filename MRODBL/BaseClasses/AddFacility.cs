a62803871f8f4c447cb5327a0697bb0da75a889d
Malay Doshi <malay.doshi@atidan.com>
2020-06-29 13:29:01 +0530

    Classes

---
+++ MRODBL/BaseClasses/AddFacility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MRODBL.BaseClasses
+{
+    public class AddFacility
+    {
+        public Facilities cFacility { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Maximum 1000 charcters")]
+        public string sConnectionString { get; set; }
+    }
+}
