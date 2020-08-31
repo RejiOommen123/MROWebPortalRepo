4621c10a0b9a004084783f001c49e44fc937bbb9
ashimagupta <ashima.gupta@razor-tech.com>
2020-08-31 15:26:19 +0530

    Master Controller created.

---
+++ MROWebApi/Controllers/MasterController.cs
@@ -0,0 +1,75 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MRODBL.BaseClasses;
+using MRODBL.BaseClassRepositories;
+using MRODBL.Entities;
+using MROWebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MROWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    [APIKeyAuth]
+    public class MasterController : ControllerBase
+    {
+        #region Master Constructor
+        private readonly DBConnectionInfo _info;
+        public MasterController(DBConnectionInfo info)
+        {
+            _info = info;
+        }
+        #endregion
+
+
+        #region Get RecordType / RecordTypes
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("[action]")]
+        public async Task<IActionResult> GetRecordTypes()
+        {
+            try
+            {
+                RecordTypesRepository rtFac = new RecordTypesRepository(_info);
+                IEnumerable<RecordTypes> recordTypes = await rtFac.GetAllASC(1000, "sRecordTypeName");
+                return Ok(recordTypes);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
+        }
+
+        [HttpGet("GetRecordType/sRecordTypeID={sRecordTypeID}&sAdminUserID={sAdminUserID}")]
+        [AllowAnonymous]
+        [Route("[action]")]
+        public async Task<ActionResult<RecordTypes>> GetRecordType(string sRecordTypeID, string sAdminUserID)
+        {
+            RecordTypesRepository rtFac = new RecordTypesRepository(_info);
+            bool resultRecordTypeID = int.TryParse(sRecordTypeID, out int nRecordTypeID);
+            bool resultAdminID = int.TryParse(sAdminUserID, out int nAdminUserID);
+            RecordTypes recordType = await rtFac.Select(nRecordTypeID);
+
+            #region Logging
+            
+            MROLogger logger = new MROLogger(_info);
+            string sDescription = "Admin with ID: " + sAdminUserID + " called Get Record Type Method for Record Type ID: " + sRecordTypeID;
+            logger.LogAdminRecords(nAdminUserID, sDescription, "Get Record Type By ID", "Master Entry - Record Type");
+           
+            #endregion
+
+            if (recordType == null)
+                return NotFound();
+            return recordType;
+        }
+        #endregion
+    }
+}
