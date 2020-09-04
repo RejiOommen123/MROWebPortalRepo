5e357ed58e2e19f5c6db83d9937dd5fa48d89b04
ashimagupta <ashima.gupta@razor-tech.com>
2020-09-04 17:25:29 +0530

    GetDisclaimer Updated

--- MROWebApi/Controllers/FacilityDisclaimersController.cs
+++ MROWebApi/Controllers/FacilityDisclaimersController.cs
@@ -22,5 +22,45 @@ namespace MROWebApi.Controllers
     [APIKeyAuth]
     public class FacilityDisclaimersController : ControllerBase
     {
+        #region Facility Disclaimers Constructor
+        private readonly DBConnectionInfo _info;
+        public FacilityDisclaimersController(DBConnectionInfo info)
+        {
+            _info = info;
+        }
+        #endregion
+
+
+        #region Get Facility Disclaimers
+        [HttpGet("GetDisclaimersByFacilityID/nFacilityID={nFacilityID}&nAdminUserID={nAdminUserID}")]
+        [AllowAnonymous]
+        [Route("[action]")]
+        public async Task<IActionResult> GetDisclaimersByFacilityID(int nFacilityID, int nAdminUserID)
+        {
+            try
+            {
+                FieldsRepository fieldsRepository = new FieldsRepository(_info);
+                IEnumerable<dynamic> fields = await fieldsRepository.EditFields(nFacilityID);
+                FacilitiesRepository rpFac = new FacilitiesRepository(_info);
+                Facilities facility = await rpFac.Select(nFacilityID);
+                if (facility == null)
+                    return NotFound();
+                var faciName = facility.sFacilityName;
+                #region Logging
+                if (facility.bFacilityLogging)
+                {
+                    MROLogger logger = new MROLogger(_info);
+                    string sDescription = "Admin with ID: " + nAdminUserID + " called Get Facility Fields Method for Facility ID: " + nFacilityID;
+                    logger.LogAdminRecords(nAdminUserID, sDescription, "Get Facility Fields By ID", "Manage Facilities");
+                }
+                #endregion
+                return Ok(new { fields, faciName });
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+        }
+#endregion
     }
 }
