dd785929ccc37508eb510a3efe1e1883068fa267
Siddhesh Lad <Siddhesh.Lad@atidan.com>
2020-09-21 18:25:08 +0530

    Audit Report changes start date and end date

--- MROAdminApplication/src/components/report/AuditReport.vue
+++ MROAdminApplication/src/components/report/AuditReport.vue
@@ -1,83 +1,99 @@
 <template>
-  <div id="PrimaryReasonPageBox">
-    <!-- Add Primary Reason Button which will redirect to Add Primary Reason Page -->
+  <div id="AuditReportPageBox">
     <v-card class="mb-5 pa-3">
-    <v-row no-gutters>
-      <v-col cols="12" sm="12" md="1" class="ml-4">
-        <v-menu v-model="menu1" :close-on-content-click="false" max-width="290">
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              transition="scale-transition"
-              offset-y
-              :value="dtRecordRangeStartFormatted"
-              :error-messages="dtRecordRangeStartErrors"
-              clearable
-              label="START DATE"
-              placeholder="MM-DD-YYYY"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-              @click:clear="dtRecordRangeStart = null"
-              @input="$v.dtRecordRangeStart.$touch()"
-              @blur="$v.dtRecordRangeStart.$touch()"
-              :disabled="bRecordMostRecentVisit || bSpecifyVisit"
-            ></v-text-field>
-          </template>
-          <v-date-picker 
-          ref="startpicker" 
-          v-model="dtRecordRangeStart" 
-          color="green lighten-1" 
-          header-color="primary" 
-          light @change="menu1 = false"
-          :max="new Date().toISOString().substr(0, 10)"
-          ></v-date-picker>
-        </v-menu>
-      </v-col>
-      <!-- End Date input -->
-      <v-col cols="12" sm="12" md="1" class="ml-4">
-        <v-menu v-model="menu2" :close-on-content-click="false" max-width="290">
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              transition="scale-transition"
-              offset-y
-              :value="dtRecordRangeEndFormatted"
-              :error-messages="dtRecordRangeEndErrors"
-              clearable
-              label="END DATE"
-               placeholder="MM-DD-YYYY"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-              @click:clear="dtRecordRangeEnd = null"
-               @input="$v.dtRecordRangeEnd.$touch()"
-              @blur="$v.dtRecordRangeEnd.$touch()"
-              :disabled="bRecordMostRecentVisit || bSpecifyVisit"
-            ></v-text-field>
-          </template>
-          <v-date-picker 
-          ref="endpicker" 
-          v-model="dtRecordRangeEnd" 
-          color="green lighten-1" 
-          header-color="primary" 
-          light 
-          @change="menu2 = false"
-          :max="new Date().toISOString().substr(0, 10)"
-          ></v-date-picker>
-        </v-menu>
-      </v-col>
-      <v-col cols="12" sm="12" md="2" class="ml-4">
-        <v-text-field label="Facility Name"></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="12" md="2" class="ml-4">
-        <v-text-field label="Location Name"></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="12" md="2" class="ml-4">
-        <v-text-field label="Admin Name"></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="12" md="2" class="ml-4">
-        <v-btn color="primary" style="margin-top: 5%;">Apply Filter</v-btn>
-      </v-col>
-    </v-row>
+      <v-row no-gutters>
+        <v-col cols="12" sm="12" md="1" class="ml-4">
+          <v-menu v-model="menu1" :close-on-content-click="false" max-width="290">
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                transition="scale-transition"
+                offset-y
+                :value="dtStartFormatted"
+                :error-messages="dtStartErrors"
+                clearable
+                label="START DATE"
+                placeholder="MM-DD-YYYY"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+                @click:clear="dtStart = null"
+                @input="$v.dtStart.$touch()"
+                @blur="$v.dtStart.$touch()"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              ref="startpicker"
+              v-model="dtStart"
+              color="green lighten-1"
+              header-color="primary"
+              light
+              @change="menu1 = false"
+              :max="new Date().toISOString().substr(0, 10)"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+        <!-- End Date input -->
+        <v-col cols="12" sm="12" md="1" class="ml-4">
+          <v-menu v-model="menu2" :close-on-content-click="false" max-width="290">
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                transition="scale-transition"
+                offset-y
+                :value="dtEndFormatted"
+                :error-messages="dtEndErrors"
+                clearable
+                label="END DATE"
+                placeholder="MM-DD-YYYY"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+                @click:clear="dtEnd = null"
+                @input="$v.dtEnd.$touch()"
+                @blur="$v.dtEnd.$touch()"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              ref="endpicker"
+              v-model="dtEnd"
+              color="green lighten-1"
+              header-color="primary"
+              light
+              @change="menu2 = false"
+              :max="new Date().toISOString().substr(0, 10)"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-col cols="12" sm="12" md="2" class="ml-4">
+          <v-text-field
+            label="Facility Name"
+            v-model="sFacilityName"
+            @input="$v.sFacilityName.$touch()"
+            @blur="$v.sFacilityName.$touch()"
+            :error-messages="sFacilityNameErrors"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="12" md="2" class="ml-4">
+          <v-text-field 
+          label="Location Name"
+          v-model="sLocationName"
+          @input="$v.sLocationName.$touch()"
+          @blur="$v.sLocationName.$touch()"
+          :error-messages="sLocationNameErrors"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="12" md="2" class="ml-4">
+          <v-text-field 
+          label="Admin Name"
+          v-model="sAdminName"
+          @input="$v.sAdminName.$touch()"
+          @blur="$v.sAdminName.$touch()"
+          :error-messages="sAdminNameErrors"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="12" md="2" class="ml-4">
+          <v-btn color="primary" style="margin-top: 5%;">Filter</v-btn>
+        </v-col>
+      </v-row>
     </v-card>
     <!-- Vuetify Card with Primary Reason List Title and Search Text Box  -->
     <v-card>
@@ -138,7 +154,11 @@
         </v-card-title>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="red darken-1" text @click="deletePrimaryReason(editedItem.nPrimaryReasonID)">Yes</v-btn>
+          <v-btn
+            color="red darken-1"
+            text
+            @click="deletePrimaryReason(editedItem.nPrimaryReasonID)"
+          >Yes</v-btn>
           <v-btn color="green darken-1" text @click="dialog = false">No</v-btn>
         </v-card-actions>
       </v-card>
@@ -168,9 +188,23 @@
 </template>
 
 <script>
+import moment from "moment";
+import { required, maxLength } from "vuelidate/lib/validators";
 export default {
   data() {
     return {
+      dtStart: moment()
+        .add(-10, "days")
+        .toISOString()
+        .substr(0, 10),
+      dtEnd: moment()
+        .toISOString()
+        .substr(0, 10),
+      sFacilityName: "",
+      sLocationName: "",
+      sAdminName: "",
+      menu1: false,
+      menu2: false,
       dialogLoader: false,
       dialog: false,
       errorAlert: false,
@@ -203,11 +237,96 @@ export default {
       },
     };
   },
+  watch: {
+    menu1(val) {
+      val && setTimeout(() => (this.$refs.startpicker.activePicker = "YEAR"));
+    },
+    menu2(val) {
+      val && setTimeout(() => (this.$refs.endpicker.activePicker = "YEAR"));
+    },
+  },
+  // Start and end date validations
+  validations: {
+    dtStart: {
+      required,
+      minValue: (value) => value < new Date().toISOString(),
+    },
+    dtEnd: {
+      required,
+      minValue: (value) => value < new Date().toISOString(),
+      afterStartDate: (value, vm) =>
+        new Date(vm.dtEnd).getTime() >= new Date(vm.dtStart).getTime(),
+    },
+    sFacilityName: {
+        maxLength: maxLength(40)
+    },
+    sLocationName: {
+        maxLength: maxLength(100)
+    },
+    sAdminName: {
+        maxLength: maxLength(50)
+    },
+  },
+  computed: {
+    //Date Format setter
+    dtStartFormatted() {
+      return this.dtStart ? moment(this.dtStart).format("MM-DD-YYYY") : "";
+    },
+    dtEndFormatted() {
+      return this.dtEnd ? moment(this.dtEnd).format("MM-DD-YYYY") : "";
+    },
+    //Start and End Date validation error message setter
+    dtStartErrors() {
+      const errors = [];
+      if (!this.$v.dtStart.$dirty) return errors;
+      !this.$v.dtStart.required && errors.push("Start Date is required");
+      !this.$v.dtStart.minValue &&
+        errors.push("This date cannot be future date");
+      return errors;
+    },
+    dtEndErrors() {
+      const errors = [];
+      if (!this.$v.dtEnd.$dirty) return errors;
+      !this.$v.dtEnd.required && errors.push("End Date is required");
+      !this.$v.dtEnd.minValue && errors.push("This date cannot be future date");
+      !this.$v.dtEnd.afterStartDate &&
+        errors.push("End Date cannot be before Start Date.");
+      return errors;
+    },
+    sFacilityNameErrors() {
+      const errors = [];
+      if (!this.$v.sFacilityName.$dirty) return errors;
+      !this.$v.sFacilityName.maxLength &&
+        errors.push("Facility Name must be at most 40 characters long");
+      return errors;
+    },
+    sLocationNameErrors() {
+      const errors = [];
+      if (!this.$v.sLocationName.$dirty) return errors;
+      !this.$v.sLocationName.maxLength &&
+        errors.push("Location Name must be at most 100 characters long");
+      return errors;
+    },
+    sAdminNameErrors() {
+      const errors = [];
+      if (!this.$v.sAdminName.$dirty) return errors;
+      !this.$v.sAdminName.maxLength &&
+        errors.push("Admin Name must be at most 50 characters long");
+      return errors;
+    },
+  },
   methods: {
     // API Call to Get all Primary Reasons
     getGridData() {
+      var adminFilterParameter ={
+        dtStart:this.dtStart,
+        dtEnd:this.dtEnd,
+        sFacilityName:this.sFacilityName,
+        sLocationName:this.sLocationName,
+        sAdminName:this.sAdminName
+      };
       this.dialogLoader = true;
-      this.$http.get("Master/GetPrimaryReasons").then(
+      this.$http.post("Report/GetAuditReport",adminFilterParameter).then(
         (response) => {
           this.gridData = JSON.parse(response.bodyText);
           this.dialogLoader = false;
@@ -221,50 +340,14 @@ export default {
           }
         }
       );
-    },
-    //Method to pass nPrimaryReasonID & sPrimaryReasonName to dialog box
-    deleteItem(nPrimaryReasonID, sPrimaryReasonName) {
-      this.editedItem.nPrimaryReasonID = nPrimaryReasonID;
-      this.editedItem.sPrimaryReasonName = sPrimaryReasonName;
-      this.dialog = true;
-    },
-    //On Agree in dialog box API Call to Delete Primary Reason
-    deletePrimaryReason(id) {
-      console.log(id);
-      this.dialogLoader = true;
-      this.dialog = false;
-      this.$http
-        .get(
-          "Master/DeletePrimaryReason/sPrimaryReasonID=" +
-            id +
-            "&sAdminUserID=" +
-            this.$store.state.adminUserId
-        )
-        .then(
-          (response) => {
-            if (response.ok == true) {
-              this.dialogLoader = false;
-              this.dialogLoader = false;
-              this.$router.go();
-            }
-          },
-          (error) => {
-            // Error Callback
-            if (error.status == 400) {
-              this.dialogLoader = false;
-              this.errorMessage = error.body;
-              this.errorAlert = true;
-            }
-          }
-        );
-    },
+    }
   },
 };
 </script>
 
 <style scoped>
 @media screen and (max-width: 500px) {
-  #PrimaryReasonPageBox {
+  #AuditReportPageBox {
     margin: 0 0em;
   }
   h1 {
