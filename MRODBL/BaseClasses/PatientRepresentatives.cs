bb5efc5154acba6bc7da83b12ef6eb3e8f45c528
Siddhesh Lad <Siddhesh.Lad@atidan.com>
2020-10-19 18:35:38 +0530

    Added Display Names for patient rep

--- MRODBL/BaseClasses/PatientRepresentatives.cs
+++ MRODBL/BaseClasses/PatientRepresentatives.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MRODBL.BaseClasses
@@ -9,13 +10,18 @@ namespace MRODBL.BaseClasses
         [Dapper.Contrib.Extensions.Key]
         [Required]
         [Range(0, int.MaxValue, ErrorMessage = "Zero & Only positive number allowed")]
+        [DisplayName("Id")]
         public int nPatientRepresentativeID { get; set; }
         [StringLength(100, ErrorMessage = "Maximum 100 characters Shipment Type allowed")]
+        [DisplayName("Name")]
         public string sPatientRepresentativeName { get; set; }
+        [DisplayName("Normalized Name")]
         public string sNormalizedPatientRepresentativeName { get; set; }
         [StringLength(500, ErrorMessage = "Maximum 500 characters Shipment Type Tool Tip allowed")]
+        [DisplayName("Tooptip")]
         public string sFieldToolTip { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "Zero & Only positive number allowed")]
+        [DisplayName("Wizard Id")]
         public int nWizardID { get; set; }
         #endregion
     }
