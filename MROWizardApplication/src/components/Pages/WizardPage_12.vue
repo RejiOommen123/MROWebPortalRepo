808e07d8c6a0dd7bd969a25b0eaff81d3f24aa3c
ashima-gupta-razor <ashima.gupta@razor-tech.com>
2021-03-31 19:40:08 +0530

    Session State Changes

--- MROWizardApplication/src/components/Pages/WizardPage_12.vue
+++ MROWizardApplication/src/components/Pages/WizardPage_12.vue
@@ -60,7 +60,7 @@ export default {
   },
   data() {
     return {
-      sSelectedSensitiveInfo: [],
+      sSelectedSensitiveInfo: this.$store.state.requestermodule.sSelectedSensitiveInfo,
        buttonKey:1,
     };
   },
