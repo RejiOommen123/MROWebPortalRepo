e5852c404cebe879a0a26a22b7c77b71c2a70e2a
Sachin Sharma <Sachin.Sharma@atidan.com>
2021-05-26 15:29:38 +0530

    ManageTextClass Rename and properties Added

---
+++ MRODBL/BaseClasses/ManageText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MRODBL.BaseClasses
+{
+   public class ManageText
+    {
+        public string sPlace { get; set; }
+        public string sTypeWithID { get; set; }
+        public string sText { get; set; }
+        public bool bReset { get; set; }
+        public string sTextType { get; set; }
+        public int nControlID { get; set; }
+        public int nCommonControlID { get; set; }
+        public int sSort { get; set; }
+
+
+    }
+}
