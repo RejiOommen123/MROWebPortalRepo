5488eace8ba1608286b68475f7541373c3ebcfbf
SIDDHESH <SIDDHESH@MUM-AT-W5-DE017>
2021-06-30 14:46:34 +0530

    Common css styling and theme

--- MROWizardApplication/src/plugins/vuetify.js
+++ MROWizardApplication/src/plugins/vuetify.js
@@ -4,7 +4,24 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        dark: true,
+  theme: {
+    themes: {
+      options: { customProperties: true,  cspNonce: 'dQw4w9WgXcQ' },
+      light: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+        customLightGrey: '#EBEEF3',
+        customText: '#334150',
+        customBlack: '#000000',
+        customWhite: '#FFFFFF',
+        customDarkBlue: '#0132D3',
+        customYellow: '#F5A707'
       },
-});
+    },
+  },
+})
