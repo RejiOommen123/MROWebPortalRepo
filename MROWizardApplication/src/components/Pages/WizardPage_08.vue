9738038dd6965cbb0f55c54ce32e6cc77a0fcb49
Ashima Gupta <ashima.gupta@razor-tech.com>
2021-07-13 17:51:07 +0530

    Screen 8 Changes

--- MROWizardApplication/src/components/Pages/WizardPage_08.vue
+++ MROWizardApplication/src/components/Pages/WizardPage_08.vue
@@ -1,251 +1,155 @@
 <template>
-  <div class="center">
-    <h1>Please specify an approximate date range for records.</h1>
-    <form>
-    <v-row>
-      <!-- Start Date input -->
-      <v-col v-if="MRORecordsDateRange" cols="12" offset-sm="1" sm="3" md="4">
-        <v-menu v-model="menu1" :close-on-content-click="false" max-width="290">
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              transition="scale-transition"
-              offset-y
-              :value="dtRecordRangeStartFormatted"
-              :error-messages="dtRecordRangeStartErrors"
-              clearable
-              label="START DATE"
-              placeholder="MM-DD-YYYY"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-              @click:clear="dtRecordRangeStart = null"
-              @input="$v.dtRecordRangeStart.$touch()"
-              @blur="$v.dtRecordRangeStart.$touch()"
-              :disabled="bRecordMostRecentVisit || bSpecifyVisit"
-            ></v-text-field>
-          </template>
-          <v-date-picker 
-          ref="startpicker" 
-          v-model="dtRecordRangeStart" 
-          color="green lighten-1" 
-          header-color="primary" 
-          light @change="menu1 = false"
-          :max="new Date().toISOString().substr(0, 10)"
-          ></v-date-picker>
-        </v-menu>
-      </v-col>
-      <!-- End Date input -->
-      <v-col v-if="MRORecordsDateRange" cols="12" offset-sm="2" sm="3" md="4">
-        <v-menu v-model="menu2" :close-on-content-click="false" max-width="290">
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              transition="scale-transition"
-              offset-y
-              :value="dtRecordRangeEndFormatted"
-              :error-messages="dtRecordRangeEndErrors"
-              clearable
-              label="END DATE"
-               placeholder="MM-DD-YYYY"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-              @click:clear="dtRecordRangeEnd = null"
-               @input="$v.dtRecordRangeEnd.$touch()"
-              @blur="$v.dtRecordRangeEnd.$touch()"
-              :disabled="bRecordMostRecentVisit || bSpecifyVisit"
-            ></v-text-field>
-          </template>
-          <v-date-picker 
-          ref="endpicker" 
-          v-model="dtRecordRangeEnd" 
-          color="green lighten-1" 
-          header-color="primary" 
-          light 
-          @change="menu2 = false"
-          :max="new Date().toISOString().substr(0, 10)"
-          ></v-date-picker>
-        </v-menu>
-      </v-col>
-      <v-col v-if="MRORecordsMostRecentVisit" cols="12" offset-sm="2" sm="8" >
-        <v-checkbox
-          hide-details
+  <div>
+       <div class="pageBody">
+      <div class="pageContent">
+    <!-- TODO:Check for subheading -->
+    <!-- <form> -->
+      <v-radio-group
+      v-model="sSelectedPrimaryReasons"
+      >
+      <!-- Get all Primary Reasons associated to facility and displayed as checkbox for selection-->
+      <v-layout
+        v-for="primaryReason in oPrimaryReasonArray"
+        :key="primaryReason.sNormalizedPrimaryReasonName"
+        row
+        wrap
+      >
+        <v-col cols="12" offset-sm="1" sm="10">
+          <v-radio
           class="checkboxBorder"
-          v-model="bRecordMostRecentVisit"
-          color="white"
-          label="Most Recent Visit."
-          @change="CheckedMostRecentVisit()"
-      ></v-checkbox>
-      </v-col>
-      <v-col v-if="MROSpecifyVisit" cols="12" offset-sm="2" sm="8" >
-        <v-checkbox
-          hide-details
-          class="checkboxBorder"
-          v-model="bSpecifyVisit"
-          color="white"
-          @change="CheckedSpecifyVisit()"
-          label="Specify Visit/Injury/Event (ER, elbow surgery, car accident, etc.)"
-      ></v-checkbox>
-      <div v-if="bSpecifyVisit">
-          <v-textarea  
-          class="TextAreaBg"  
-          v-model="sSpecifyVisitText" 
-          maxlength="100" 
-          rows="3" 
-          counter 
-          label="Specify Visit"
-          :error-messages="sSpecifyVisitTextErrors"
-          @input="$v.sSpecifyVisitText.$touch()"
-          @blur="$v.sSpecifyVisitText.$touch()"
-          ></v-textarea>
-      </div>
-      </v-col>
-      <br />
-      <v-col cols="12" offset-sm="3" sm="6" style="padding-top:0">
-        <div>
-          <v-btn v-if="bRecordMostRecentVisit"  @click.once="nextPage" :key="buttonKey" class="next">Next</v-btn>
-          <v-btn v-if="bSpecifyVisit" :disabled="sSpecifyVisitText==''" @click.once="nextPage" :key="buttonKey" class="next">Next</v-btn>
-          <v-btn v-if="!bRecordMostRecentVisit && !bSpecifyVisit" :disabled="$v.dtRecordRangeStart.$invalid || $v.dtRecordRangeEnd.$invalid" @click.once="nextPage" :key="buttonKey"  class="next">Next</v-btn>
+          color="black"
+          :label="primaryReason.sPrimaryReasonName"
+          :value="primaryReason.sNormalizedPrimaryReasonName"
+           @change="check(primaryReason)"
+          >
+          <template v-slot:label>
+            <v-col cols="11" sm="11" style="padding:0px">
+              <span class="checkboxLabel">{{primaryReason.sPrimaryReasonName}}</span>
+            </v-col>
+            <v-col cols="1" sm="1" style="padding:0px">
+            <v-tooltip v-if="primaryReason.sFieldToolTip" slot="append" left>
+              <template v-slot:activator="{ on }">
+                <v-icon v-on="on" color="black" top>mdi-information</v-icon>
+              </template>
+              <v-col cols="12" sm="12">
+                <p style="width:200px; background-color:black;color:white">{{primaryReason.sFieldToolTip}}</p>
+              </v-col>
+            </v-tooltip>
+            </v-col>
+          </template>
+          </v-radio>
+        </v-col> 
+      </v-layout>
+      </v-radio-group>
+      <!-- If requester selects other reason then free text box will appear to enter data -->
+      <v-col cols="12" offset-sm="3" sm="6">
+        <div v-if="this.bOther==true">
+          <v-textarea class="TextAreaBg"  v-model="sOtherPrimaryReasons" maxlength="100" rows="3" counter label="Other Reason"></v-textarea>
         </div>
       </v-col>
+    <!-- </form> -->
+     <h6 id="subHeading" v-if="disclaimer!=''">{{disclaimer}}</h6>
+       <Footer v-if="$vuetify.breakpoint.xs" />
+      </div>
+       </div>
+       <div :class="$vuetify.breakpoint.xs ? 'buttonBlockMobile' : 'buttonBlock'">
+<v-row>
+   <v-col cols="6" sm="2">
+      <v-btn @click.once="skipPage" :key="buttonKey" class="smallWhiteBtn">Skip</v-btn>
+    </v-col>
+    <v-col cols="6" offset-sm="4" sm="2">
+      <v-btn :disabled="sSelectedPrimaryReasons[0]==null" @click.once="nextPage" :key="buttonKey" class="smallBlackBtn">Continue</v-btn>
+    </v-col>
     </v-row>
-    </form>
-    </div>
-    
+       </div>
+        <Footer v-if="!$vuetify.breakpoint.xs" />
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import moment from "moment";
-import { required} from "vuelidate/lib/validators";
+import Footer from "../Footer.vue";
 export default {
   name: "WizardPage_08",
-   activated(){
+  activated(){
     this.buttonKey++;
-   },
+    this.$store.commit(
+      "ConfigModule/titleQuestion",
+      "What is the primary reason for requesting records?"
+    );
+    if(this.sSelectedStatePrimaryReasons.length == 0){
+      this.bOther = false;
+      this.sSelectedPrimaryReasons = [];
+      this.sOtherPrimaryReasons = '';
+      this.sSelectedPrimaryReasonsName = '';
+    }
+  },
+   components: {
+    Footer,
+  },
+  computed:{
+    ...mapState({
+      oPrimaryReasonArray : state => state.ConfigModule.apiResponseDataByLocation.oPrimaryReason,
+      disclaimer : state => state.ConfigModule.apiResponseDataByFacilityGUID.wizardHelper.Wizard_13_disclaimer01,
+      sSelectedStatePrimaryReasons : state => state.requestermodule.sSelectedPrimaryReasons,
+    }),
+  },
   data() {
     return {
-      dtRecordRangeStart: this.$store.state.requestermodule.dtRecordRangeStart,
-      dtRecordRangeEnd: this.$store.state.requestermodule.dtRecordRangeEnd,
-      bRecordMostRecentVisit: this.$store.state.requestermodule.bRecordMostRecentVisit,
-      bSpecifyVisit: this.$store.state.requestermodule.bSpecifyVisit,
-      sSpecifyVisitText: this.$store.state.requestermodule.sSpecifyVisitText,
-      menu1: false,
-      menu2: false,
-      buttonKey:1,
+      bOther: this.$store.state.requestermodule.sSelectedPrimaryReasons=="MROOtherPrimaryReason" ? true : false,
+      sSelectedPrimaryReasons: this.$store.state.requestermodule.sSelectedPrimaryReasons,
+      sOtherPrimaryReasons: this.$store.state.requestermodule.sSelectedPrimaryReasons=="MROOtherPrimaryReason" ? this.$store.state.requestermodule.sSelectedPrimaryReasonsName : '',
+      sSelectedPrimaryReasonsName:this.$store.state.requestermodule.sSelectedPrimaryReasonsName,
+        buttonKey:1,
     };
   },
-   watch: {
-      menu1 (val) {
-        val && setTimeout(() => (this.$refs.startpicker.activePicker = 'YEAR'))
-      },
-      menu2 (val) {
-        val && setTimeout(() => (this.$refs.endpicker.activePicker = 'YEAR'))
-      },
-    },
-  // Start and end date validations
-  validations: {
-    dtRecordRangeStart: {
-        required,
-        minValue: value => value < new Date().toISOString()
-    },
-    dtRecordRangeEnd: {
-        required,
-        minValue: value => value < new Date().toISOString(),
-        afterStartDate: (value,vm) => new Date(vm.dtRecordRangeEnd).getTime() >= new Date(vm.dtRecordRangeStart).getTime()
-    },
-    sSpecifyVisitText :{
-        required
-    },
-  },
   methods: {
-    CheckedMostRecentVisit(){
-      if(this.bRecordMostRecentVisit)
-      {
-        this.$v.dtRecordRangeStart.$reset();
-        this.$v.dtRecordRangeEnd.$reset();
-        this.dtRecordRangeStart=null;
-        this.dtRecordRangeEnd=null;
-        this.bSpecifyVisit=false;
-        this.sSpecifyVisitText="";
-        this.$v.sSpecifyVisitText.$reset();
-      }
-    },
-    CheckedSpecifyVisit() {
-      if(this.bSpecifyVisit)
-      {
-        this.$v.dtRecordRangeStart.$reset();
-        this.$v.dtRecordRangeEnd.$reset();
-        this.dtRecordRangeStart=null;
-        this.dtRecordRangeEnd=null;
-        this.bRecordMostRecentVisit=false;
-      }
-      if (!this.bSpecifyVisit) {
-        this.sSpecifyVisitText = "";
-      }
-    },   
     nextPage() {
-      // if(this.bRecordMostRecentVisit){
-      //   this.$store.commit("requestermodule/bRecordMostRecentVisit", this.bRecordMostRecentVisit);
-      //   this.$store.commit("requestermodule/dtRecordRangeStart", '');
-      //   this.$store.commit("requestermodule/dtRecordRangeEnd", '');
-      // }
-      // else{
-      //   this.$store.commit("requestermodule/bRecordMostRecentVisit", this.bRecordMostRecentVisit);
-      //   this.$store.commit("requestermodule/dtRecordRangeStart", this.dtRecordRangeStart);
-      //   this.$store.commit("requestermodule/dtRecordRangeEnd", this.dtRecordRangeEnd);
-      // }
-      
-      this.$store.commit("requestermodule/dtRecordRangeStart", this.dtRecordRangeStart);
-      this.$store.commit("requestermodule/dtRecordRangeEnd", this.dtRecordRangeEnd);
-      this.$store.commit("requestermodule/bRecordMostRecentVisit", this.bRecordMostRecentVisit);
-      this.$store.commit("requestermodule/bSpecifyVisit", this.bSpecifyVisit);
-      this.$store.commit("requestermodule/sSpecifyVisitText", this.sSpecifyVisitText);
+      this.$store.commit(
+        "requestermodule/sSelectedPrimaryReasons",
+        this.sSelectedPrimaryReasons
+      );
+        if (this.sSelectedPrimaryReasons == "MROOtherPrimaryReason") {
+          this.sSelectedPrimaryReasonsName=this.sOtherPrimaryReasons;
+        }
+      this.$store.commit("requestermodule/sSelectedPrimaryReasonsName", this.sSelectedPrimaryReasonsName);
 
       //Partial Requester Data Save Start
       this.$store.dispatch('requestermodule/partialAddReq');
-    
+
       this.$store.commit("ConfigModule/mutateNextIndex");
+    },
+    skipPage(){
+      this.sSelectedPrimaryReasons = [];
+      this.bOther=false;
+      this.$store.commit("requestermodule/sSelectedPrimaryReasons",[]);
+      this.$store.commit("requestermodule/sSelectedPrimaryReasonsName", '');
+      this.$store.commit("ConfigModule/mutateNextIndex");
+    },
+    // to check if selected checkbox is other reason
+    check(primaryReason) {
+        this.sSelectedPrimaryReasons = [];
+        this.sSelectedPrimaryReasons.push(primaryReason.sNormalizedPrimaryReasonName);
+        this.sSelectedPrimaryReasonsName=primaryReason.sPrimaryReasonName;
+        if (this.sSelectedPrimaryReasons == "MROOtherPrimaryReason") {
+          this.bOther = true;
+          this.sSelectedPrimaryReasonsName=this.sOtherPrimaryReasons;
+        }
+        else{
+          this.bOther=false;
+          this.sOtherPrimaryReasons='';
+        }
+          
     }
   },
-  computed: {
-     //Date Format setter
-    dtRecordRangeStartFormatted() {
-      return this.dtRecordRangeStart ? moment(this.dtRecordRangeStart).format("MM-DD-YYYY") : "";
-    },
-    dtRecordRangeEndFormatted() {
-      return this.dtRecordRangeEnd ? moment(this.dtRecordRangeEnd).format("MM-DD-YYYY") : "";
-    },
-     //Start and End Date validation error message setter
-    dtRecordRangeStartErrors(){
-      const errors = [];
-      if (!this.$v.dtRecordRangeStart.$dirty) return errors;
-      !this.$v.dtRecordRangeStart.required && errors.push("Start Date is required");
-      !this.$v.dtRecordRangeStart.minValue && errors.push("This date cannot be future date");    
-      return errors;
-    },
-     dtRecordRangeEndErrors(){
-      const errors = [];
-      if (!this.$v.dtRecordRangeEnd.$dirty) return errors;
-       !this.$v.dtRecordRangeEnd.required && errors.push("End Date is required");
-      !this.$v.dtRecordRangeEnd.minValue && errors.push("This date cannot be future date");
-      !this.$v.dtRecordRangeEnd.afterStartDate && errors.push("End Date cannot be before Start Date.");
-      return errors;
-    },
-     sSpecifyVisitTextErrors(){
-      const errors = [];
-      if (!this.$v.sSpecifyVisitText.$dirty) return errors;
-      !this.$v.sSpecifyVisitText.required && errors.push("Specify visit is required.");
-      return errors;
-    },    
-    ...mapState({
-      //Show and Hide Fields Values
-      MRORecordsDateRange: state => state.ConfigModule
-      .apiResponseDataByLocation.oFields.MRORecordsDateRange,
-      MRORecordsMostRecentVisit: state => state.ConfigModule
-      .apiResponseDataByLocation.oFields.MRORecordsMostRecentVisit,
-      MROSpecifyVisit: state => state.ConfigModule
-      .apiResponseDataByLocation.oFields.MROSpecifyVisit,
-    }),
-  }
 };
 </script>
+<style scoped>
+.v-tooltip__content{
+  color: black;
+  background: white;
+}
+#subHeading{
+  margin-bottom:5%
+}
+</style>
